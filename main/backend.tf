0792cf975cc17b7d0a95abf25f597432d4cdda51
marinahambaryan <marinahambaryan99@gmail.com>
2024-10-31 21:10:46 +0400

    change my bocket name

--- main/backend.tf
+++ main/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "aca-terraform-states"
+    bucket         = "terraform-states-for-aca"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
     key            = "projects/main-terraform-states.tfstate"
